d55ac603e9e477b57dd27e9cb2ab976e001bad50
Thomas Chu <itsthomaschu@gmail.com>
2018-03-27 01:44:00 -0600

    completed main functionality of attributeClosures()

--- main.py
+++ main.py
@@ -3,15 +3,17 @@ import sqlite3
 
 def test():
     global connection, cursor
-    db_file = input('Enter input database file: ')
+    #db_file = input('Enter input database file: ')
+    db_file = 'mini-project2-Example.sqliteDB'
     connection = sqlite3.connect(db_file)
     cursor = connection.cursor()
     cursor.row_factory = sqlite3.Row
 
-    cursor.execute('SELECT FDs FROM InputRelationSchemas WHERE Name = "Person"')
-    result = get_func_dependencies(cursor.fetchone()['FDs'])
-    print(result)
-    print(getClosure('SSN', result))
+    cursor.execute('SELECT Name FROM InputRelationSchemas')
+    for row in cursor.fetchall():
+        print(row['Name'])
+
+    attributeClosures()
 
     return
 
@@ -38,14 +40,7 @@ def main():
             convertToBCNF()
             continue
         elif(choice == 2):
-            # get inputs
-            attr_input = input('Enter attribute to calculate closure: ')
-            tables = input('Enter tables to compute attribute closures: ')
-            tables.split(',')
-            for table in tables:
-                cursor.execute('SELECT FD FROM ?', (table,))
-                result = getAttrClosure(attr_input, cursor.fetchone()['FD'])
-
+            pass
             
             continue
         elif(choice == 3):
@@ -66,10 +61,38 @@ def checkEquivalence(tbl1, tbl2):
     # if F1+ == F2+ then return true else false
     pass
 
+def attributeClosures():
+    # get input schema and attributes to calculate
+    schema_input = input('Enter schemas for FD: ')
+    attr_input = input('Enter attributes to compute closure: ')
+
+    # parses attribute and converts to a list
+    attr_input = parseAttributesInput(attr_input)
+    print(attr_input)
+
+    schema_input = schema_input.split(',')
+    fd_input = []
+    for schema in schema_input:
+        cursor.execute('SELECT FDs FROM InputRelationSchemas WHERE Name = ?', (schema,))
+        fd_input.append(cursor.fetchone()['FDs'])
+    
+    # join all FD's into a string to parse
+    fd_input = '; '.join(fd_input)
+
+    # parse FDs from database to dictionary and list attributes
+    func_dependencies = get_func_dependencies(fd_input)
+
+    # for every attribute calculate its closure
+    for attribute in attr_input:
+       print('{} -> {}'.format(attribute, getClosure(attribute, func_dependencies)))
+    return
+
 def getClosure(attribute, func_dependencies):
+    # given an attribute and a functional dependencies list return a list of the attributes closure
     closure = set(attribute.split(','))
     explored = set()
 
+    # once we cant find any unique closures exit loop
     while(explored != closure):
         explored = closure
 
@@ -77,10 +100,12 @@ def getClosure(attribute, func_dependencies):
             if(set(lhs.split(',')).issubset(closure)):
                 # closure = closure U RHS
                 closure = closure.union(func_dependencies[lhs].split(','))
-  
-    return closure
+    
+    # returns sorted closure alphabetically
+    return sorted(closure, key = lambda x: ord(x))
 
 def get_func_dependencies(FD_list):
+    # given a string of FDs from database, return a dictionary of the parsed FD
 
     # create a dictionary to store function dependencies
     func_dependencies = {}
@@ -101,6 +126,27 @@ def get_func_dependencies(FD_list):
 
     return func_dependencies
 
+def parseAttributesInput(attr_input):
+    # given a string of attr_input convert to a list
+    
+    # replace inner comma with ; as placeholder
+    # split string into list
+    attr_input = attr_input.replace(', ', ';')
+    attr_input = attr_input.split(',')
+    out = []
+
+    for attr in attr_input:
+        # remove {, }, ; keeping only attributes
+        attr = attr.replace('{', '')
+        attr = attr.replace('}', '')
+        attr = attr.replace(';', ',')
+        out.append(attr)
+    
+    # returns a list of attributes from input string
+    return out
+
+        
+        
 
 
 
